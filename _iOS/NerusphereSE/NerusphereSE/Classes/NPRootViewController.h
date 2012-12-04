7347cc74c4c3b9f1f28cf4c63f3d74533378a099
Mario Gonzalez <mariogonzalez@gmail.com>
2012-12-04 14:30:17 -0500

    Box2D compiles with project

---
+++ _iOS/NerusphereSE/NerusphereSE/Classes/NPRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  NPRootViewController.h
+//  NerusphereSE
+//
+//  Created by Mario Gonzalez on 12/4/12.
+//  Copyright (c) 2012 Mario Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPRootViewController : UIViewController
+
+@end
---
+++ _iOS/NerusphereSE/NerusphereSE/Classes/NPRootViewController.mm
@@ -0,0 +1,35 @@
+//
+//  NPRootViewController.m
+//  NerusphereSE
+//
+//  Created by Mario Gonzalez on 12/4/12.
+//  Copyright (c) 2012 Mario Gonzalez. All rights reserved.
+//
+
+#import "NPRootViewController.h"
+
+@interface NPRootViewController ()
+
+@end
+
+@implementation NPRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
